201ff606abb3bf5a75dd1e88645b5b4003a3b76e
Connor <connora.wellington18@gmail.com>
2020-09-10 21:02:52 +1200

    Added Sign Up functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,28 @@ class UsersController < ApplicationController
     end
     
     def new
+        @user = User.new
+    end
 
+    def create
+        # Pass parameters to a new User object
+        @user = User.new(user_params)
+        puts user_params
+
+        if @user.save
+            # If user is successfully saved redirect to user show page
+            redirect_to "/users/#{@user.id}"
+        else
+            # If the user could not be saved, display errors
+            render users_new_path
+        end
     end
 
     def show
 
+    end
+
+    def user_params
+        return params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#welcome'
   get '/welcome', to: 'users#welcome'
   get '/users/new', to: 'users#new'
+  post '/users/create', to: 'users#create'
   get '/users/:id', to: 'users#show'
 end
