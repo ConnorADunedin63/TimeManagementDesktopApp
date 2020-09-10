823e02a828b34071a2ab99dd589616c81f7d2994
Connor <connora.wellington18@gmail.com>
2020-09-10 15:57:36 +1200

    Added user model and welcome page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+    def welcome
+
+    end
+    
+    def new
+
+    end
+
+    def show
+
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/welcome', to: 'users#welcome'
+  get '/users/new', to: 'users#new'
+  get '/users/:id', to: 'users#show'
 end
