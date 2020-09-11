89b45250d96a5eb19ad13aee7e630414f03df76b
Connor <connora.wellington18@gmail.com>
2020-09-11 19:02:01 +1200

    Added authentication action and error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
     def create
         # Pass parameters to a new User object
         @user = User.new(user_params)
-        puts user_params
 
         if @user.save
             # If user is successfully saved redirect to user show page
@@ -21,8 +20,34 @@ class UsersController < ApplicationController
         end
     end
 
-    def login
+    def login 
+    end
 
+    def authenticate
+        # Both the email and password should be present
+        unless params[:email].present? && params[:password].present?
+            if !params[:email].present?
+                flash[:blank_email] = "Email cannot be blank!"
+            end
+    
+            if !params[:password].present?
+                flash[:blank_password] = "Password cannot be blank!"
+            end
+            redirect_to users_login_path
+            return
+        end
+
+        @user = User.find_by(email: params[:email])
+
+        if @user.present?
+            # If the user with the email is present, try authenticating with password
+            if @user.authenticate(params[:password]) == false
+                # Redirect to the user's show page
+                redirect_to "/users/#{@user.id}"
+            end
+        end
+        # Incorrect email or password
+        redirect_to users_login_path, alert: "Incorrect email or password"
     end
 
     def show
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#welcome'
   get '/welcome', to: 'users#welcome'
+
   get '/users/new', to: 'users#new'
-  get '/users/login', to: 'users#login'
   post '/users/create', to: 'users#create'
+  
+  get '/users/login', to: 'users#login'
+  post '/users/login', to: 'users#authenticate', as: 'users_authentication'
+
   get '/users/:id', to: 'users#show'
 end
