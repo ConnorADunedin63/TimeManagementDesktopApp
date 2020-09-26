db64d02bf39c5e896c43f916f04ddc33e4713c83
Connor <connora.wellington18@gmail.com>
2020-09-26 17:28:45 +1200

    Added goal scopes

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,9 @@
 class Goal < ApplicationRecord
+    scope :short_term, -> { where("? - due_date < ?", Time.zone.now,  7862400.0) }
+    scope :long_term, -> { where("? - due_date >= ?", Time.zone.now,  7862400.0) }
+    scope :ongoing, -> { where("due_date IS NIL")}
+    scope :due, -> { where("due_date > ?"), Time.zone.now}
+    
     belongs_to :user
     validates :name, presence: true
 end
